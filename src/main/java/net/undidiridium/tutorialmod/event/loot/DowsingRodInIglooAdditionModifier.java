d31984df3436fd91c20448c181ef84ee29ba8bdb
Undid Iridium <FrancisXIrizarry@gmail.com>
2023-02-02 00:37:07 -0500

    Testing variations

--- src/main/java/net/undidiridium/tutorialmod/event/loot/DowsingRodInIglooAdditionModifier.java
+++ src/main/java/net/undidiridium/tutorialmod/event/loot/DowsingRodInIglooAdditionModifier.java
@@ -1,11 +1,15 @@
 package net.undidiridium.tutorialmod.event.loot;
 
 import com.google.gson.JsonObject;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.TextComponent;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.util.GsonHelper;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.storage.loot.LootContext;
+import net.minecraft.world.level.storage.loot.parameters.LootContextParams;
 import net.minecraft.world.level.storage.loot.predicates.LootItemCondition;
 import net.minecraftforge.common.loot.GlobalLootModifierSerializer;
 import net.minecraftforge.common.loot.LootModifier;
@@ -25,6 +29,13 @@ public class DowsingRodInIglooAdditionModifier extends LootModifier {
     @Nonnull
     @Override
     protected List<ItemStack> doApply(final List<ItemStack> generatedLoot, final LootContext context) {
+        if (context.hasParam(LootContextParams.BLOCK_ENTITY)) {
+            final BlockPos chestPos = context.getParam(LootContextParams.BLOCK_ENTITY).getBlockPos();
+            final Player player = context.getParam(LootContextParams.THIS_ENTITY).level.getNearestPlayer(context.getParam(LootContextParams.THIS_ENTITY), 10);
+            player.sendMessage(new TextComponent("Found " + chestPos), player.getUUID());
+        }
+        final Player player = context.getParam(LootContextParams.THIS_ENTITY).level.getNearestPlayer(context.getParam(LootContextParams.THIS_ENTITY), 10);
+        player.sendMessage(new TextComponent("Entity not found "), player.getUUID());
         generatedLoot.add(new ItemStack(this.addition, 1));
         return generatedLoot;
     }
